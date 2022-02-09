f2c0751562a417a51a576a985f33c56c7e8228cf
Alexander <ivalester@mail.ru>
2022-02-09 15:58:09 +0300

    User methods

--- api/resources/user.py
+++ api/resources/user.py
@@ -5,6 +5,10 @@ from api.models.user import UserModel
 
 class UserResource(Resource):
     def get(self, user_id=None):
+        if user_id is None:
+            users = UserModel.query.all()
+            return [user_schema.dump(user) for user in users]
+                
         user = UserModel.query.get(user_id)
         if not user:
             return {"Error": f"User with id={user_id} not found"}, 404
@@ -20,3 +24,25 @@ class UserResource(Resource):
         db.session.commit()
         return user_schema.dump(user), 201
 
+    def put(self, user_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("username")
+        parser.add_argument("password")
+        user_data = parser.parse_args()
+        user = UserModel.query.get(user_id)
+        if user is None:
+            return f"User with id {user_id} not found", 404
+        if user_data["username"]:
+            user.username = user_data["username"]
+        if user_data["password"]:
+            user.hash_password(user_data["password"])
+        db.session.commit()
+        return user_schema.dump(user), 200
+
+    def delete(self, user_id):
+        user = UserModel.query.get(user_id)
+        if user is None:
+            return f"User with id {user_id} not found", 404
+        db.session.delete(user)
+        db.session.commit()
+        return user_schema.dump(user), 200
